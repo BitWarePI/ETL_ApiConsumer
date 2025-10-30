10bf0e7b81df366d0c07a10ce8968b5d509ef79d
muniz <gustavo.doliveira@sptech.school>
2025-10-30 10:18:41 -0300

    Load da ETL concluido

--- src/main/java/com/bitcoin/pi/BitwareSQL.java
+++ src/main/java/com/bitcoin/pi/BitwareSQL.java
@@ -12,10 +12,10 @@ public class BitwareSQL {
             String url = "jdbc:mysql://localhost:3306/bitware_db";
 
             // MUDAR AQUIIII NA HORA DE TESTARRR!!!!!!!!!!!!!
-            String user = "aluno";
-            String password = "123456";
-//            String user = "bitware";
-//            String password = "sptech";
+//            String user = "aluno";
+//            String password = "123456";
+            String user = "bitware";
+            String password = "sptech";
 
             this.conn = DriverManager.getConnection(url, user, password);
             System.out.println("Conectado ao MySQL com sucesso!!!!!!!!!!!!!!!");
@@ -24,14 +24,24 @@ public class BitwareSQL {
         }
     }
 
-    public boolean CompararBanco(Integer idMaquina, String enderecoMac, Integer fkEmpresa) {
+    public int CompararBanco(String enderecoMac) {
         if (conn == null) {
             System.out.println("Conexão com o banco não inicializada!!!!!!!!!!");
-            return(false);
+            return 0;
         }
         try {
-            String sqlSelect = "SELECT maquina.idMaquina, maquina.enderecoMac, empresa.nome FROM bitware_db.Maquina as maquina INNER JOIN \n" +
-                    " bitware_db.Empresa as empresa ON maquina.fkEmpresa = empresa.idEmpresa WHERE enderecoMac = ?;";
+            String sqlSelect = """
+                SELECT 
+                    maquina.idMaquina, 
+                    maquina.enderecoMac, 
+                    maquina.fkEmpresa,
+                    empresa.nome 
+                FROM bitware_db.Maquina AS maquina 
+                INNER JOIN bitware_db.Empresa AS empresa 
+                ON maquina.fkEmpresa = empresa.idEmpresa 
+                WHERE maquina.enderecoMac = ?;
+                """;
+
             PreparedStatement ps = conn.prepareStatement(sqlSelect);
 
 
@@ -40,25 +50,26 @@ public class BitwareSQL {
             ResultSet rs = ps.executeQuery();
 
             if (rs.next()) {
-                int idMaquinaQuery = rs.getInt("idMaquina");
+              //  int idMaquinaQuery = rs.getInt("idMaquina");
+                int fkEmpresaQuery = rs.getInt("fkEmpresa");
                 String nomeEmpresaQuery = rs.getString("nome");
 
                 System.out.println("Máquina com o endereço MAC " + enderecoMac + " existe no banco de dados!");
-                System.out.println("ID da máquina: " + idMaquinaQuery);
-                System.out.println("Pertence à empresa: " + nomeEmpresaQuery);
+               // System.out.println("ID da máquina: " + idMaquinaQuery);
+                System.out.println("Pertence à empresa (id): " + fkEmpresaQuery + " - " + nomeEmpresaQuery);
                 System.out.println();
                 rs.close();
                 ps.close();
-                return(true);
+                return fkEmpresaQuery;
             } else {
                 rs.close();
                 ps.close();
-                return(false);
+                return 0;
             }
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Ocorreu um erro ao tentar selecionar a máquina!!!!!!!!!");
-            return(false);
+            return 0;
         }
     }
 }
--- src/main/java/com/bitcoin/pi/Maquina.java
+++ src/main/java/com/bitcoin/pi/Maquina.java
@@ -1,45 +1,111 @@
 package com.bitcoin.pi;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class Maquina {
     private Integer id;
     private String enderecoMac;
     private int fkEmpresa;
 
+    // Novos atributos para as leituras de dados
+    private String dataHoraLeitura;
+    private Double usoGpu;
+    private Double usoCpu;
+    private Double tempGpu;
+    private Double tempCpu;
+
     public Maquina() {}
 
-    public Maquina(Integer id, String enderecoMac, Integer fkEmpresa) {
+    // Construtor completo (opcional, mas bom para ter)
+    public Maquina(Integer id, String enderecoMac, Integer fkEmpresa,
+                   String dataHoraLeitura, Double usoGpu, Double usoCpu,
+                   Double tempGpu, Double tempCpu) {
         this.id = id;
         this.enderecoMac = enderecoMac;
         this.fkEmpresa = fkEmpresa;
+        this.dataHoraLeitura = dataHoraLeitura;
+        this.usoGpu = usoGpu;
+        this.usoCpu = usoCpu;
+        this.tempGpu = tempGpu;
+        this.tempCpu = tempCpu;
     }
 
     public Maquina(String enderecoMac) {
         this.enderecoMac = enderecoMac;
     }
 
+    // --- Getters e Setters existentes ---
     public Integer getId() {
         return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getEnderecoMac() {
         return enderecoMac;
     }
 
+    public void setEnderecoMac(String enderecoMac) {
+        this.enderecoMac = enderecoMac;
+    }
+
     public int getFkEmpresa() {
         return fkEmpresa;
     }
 
-    public void setEnderecoMac(String enderecoMac) {
-        this.enderecoMac = enderecoMac;
+    public void setFkEmpresa(int fkEmpresa) {
+        this.fkEmpresa = fkEmpresa;
+    }
+
+    // --- Novos Getters e Setters ---
+    public String getDataHoraLeitura() {
+        return dataHoraLeitura;
+    }
+
+    public void setDataHoraLeitura(String dataHoraLeitura) {
+        this.dataHoraLeitura = dataHoraLeitura;
+    }
+
+    public Double getUsoGpu() {
+        return usoGpu;
+    }
+
+    public void setUsoGpu(Double usoGpu) {
+        this.usoGpu = usoGpu;
+    }
+
+    public Double getUsoCpu() {
+        return usoCpu;
+    }
+
+    public void setUsoCpu(Double usoCpu) {
+        this.usoCpu = usoCpu;
+    }
+
+    public Double getTempGpu() {
+        return tempGpu;
+    }
+
+    public void setTempGpu(Double tempGpu) {
+        this.tempGpu = tempGpu;
+    }
+
+    public Double getTempCpu() {
+        return tempCpu;
+    }
+
+    public void setTempCpu(Double tempCpu) {
+        this.tempCpu = tempCpu;
     }
 
     @Override
@@ -48,38 +114,91 @@ public class Maquina {
                 "id=" + id +
                 ", enderecoMac='" + enderecoMac + '\'' +
                 ", fkEmpresa=" + fkEmpresa +
+                ", dataHoraLeitura='" + dataHoraLeitura + '\'' +
+                ", usoGpu=" + usoGpu +
+                ", usoCpu=" + usoCpu +
+                ", tempGpu=" + tempGpu +
+                ", tempCpu=" + tempCpu +
                 '}';
     }
 
     public void lerCsv() {
-        String nomeArquivo = "src/leituras.csv";
-        //parte para quebrar as colunas do csv pra pegar so o endereco mac
+        String arquivoEntrada = "src/leituras.csv";
+        String arquivoSaida = "src/maquinas_validadas.csv";
         String separador = ";";
-        //random para adicionar o id nas maquinas que podem ou nao bater com oq ta no banco
-        Random random = new Random();
+
         BitwareSQL banco = new BitwareSQL();
+
         List<Maquina> maquinasValidadas = new ArrayList<>();
 
         try (BufferedReader reader = new BufferedReader(
-                new InputStreamReader(new FileInputStream(nomeArquivo), StandardCharsets.UTF_8 ))) {
+                new InputStreamReader(new FileInputStream(arquivoEntrada), StandardCharsets.UTF_8))) {
+
+            // Pula o cabeçalho
+            reader.readLine();
+
             String linha;
-            System.out.println("Conteúdo do arquivo: " + nomeArquivo);
-            while ((linha = reader.readLine()) != null){
+            while ((linha = reader.readLine()) != null) {
                 String[] dados = linha.split(separador);
 
-                String enderecoMac = dados[8];
-                Maquina maquina = new Maquina(enderecoMac);
+                if (dados.length < 9) continue;
 
-                boolean existe = banco.CompararBanco(maquina.getId(), maquina.getEnderecoMac(), maquina.getFkEmpresa());
-                if(existe){
+                String dataHora = dados[0];
+                String usoCpu = dados[2];
+                String usoGpu = dados[3];
+                String tempCpu = dados[6];
+                String tempGpu = dados[7];
+                String enderecoMac = dados[8];
+
+                Maquina maquina = new Maquina(enderecoMac);
+                maquina.setDataHoraLeitura(dataHora);
+                maquina.setUsoCpu(Double.parseDouble(usoCpu.replace(",", ".")));
+                maquina.setUsoGpu(Double.parseDouble(usoGpu.replace(",", ".")));
+                maquina.setTempCpu(Double.parseDouble(tempCpu.replace(",", ".")));
+                maquina.setTempGpu(Double.parseDouble(tempGpu.replace(",", ".")));
+
+
+                int fkEmpresa = banco.CompararBanco(enderecoMac);
+
+                if (fkEmpresa > 0) {
+                    // Se CompararBanco retornou true, o csv foi preenchido.
                     maquinasValidadas.add(maquina);
+                    maquina.setFkEmpresa(fkEmpresa);
+                } else {
+                    System.out.println("Máquina ignorada (não encontrada no banco): " + enderecoMac);
                 }
             }
 
-            System.out.println("\n\n"+maquinasValidadas);
-        }catch (IOException e){
-            System.out.println("Erro ao ler o arquivo: " + nomeArquivo);
+
+            escreverCsvValidado(arquivoSaida, maquinasValidadas, separador);
+            System.out.println("Arquivo gerado com sucesso: " + arquivoSaida);
+
+        } catch (Exception e) {
+            System.out.println("Erro ao processar o arquivo: " + e.getMessage());
+        }
+    }
+
+    // --- ESCREVER CSV ---
+    private void escreverCsvValidado(String nomeArquivo, List<Maquina> maquinas, String separador) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(nomeArquivo))) {
+
+            writer.write("datetime;uso_cpu;uso_gpu;temp_cpu;temp_gpu;mac_address;fk_empresa");
+            writer.newLine();
+
+            for (Maquina m : maquinas) {
+                System.out.println("FK EMPRESA: " + m.getFkEmpresa());
+                writer.write(m.getDataHoraLeitura() + separador +
+                        m.getUsoCpu() + separador +
+                        m.getUsoGpu() + separador +
+                        m.getTempCpu() + separador +
+                        m.getTempGpu() + separador +
+                        m.getEnderecoMac() + separador +
+                        m.getFkEmpresa());
+                writer.newLine();
+            }
+
+        } catch (IOException e) {
+            System.out.println("Erro ao salvar o arquivo: " + e.getMessage());
         }
     }
 }
-
